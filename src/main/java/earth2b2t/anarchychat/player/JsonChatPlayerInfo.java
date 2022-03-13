afec7d5483e051a06a983dad8e9701cf57690ab9
Toshimichi0915 <main@toshimichi.net>
2022-03-13 09:04:52 +0900

    Remove soft ignore on logout

--- src/main/java/earth2b2t/anarchychat/player/JsonChatPlayerInfo.java
+++ src/main/java/earth2b2t/anarchychat/player/JsonChatPlayerInfo.java
@@ -11,13 +11,18 @@ import java.util.UUID;
 public class JsonChatPlayerInfo {
     private final UUID uniqueId;
     private final String name;
-    private final List<Ignore> ignoreList;
+    private final List<String> ignoreList;
 
     public JsonChatPlayerInfo(JsonChatPlayer player) {
-        this(player.getUniqueId(), player.getName(), player.getIgnoreList());
+        this(player.getUniqueId(), player.getName(),
+                player.getIgnoreList().stream()
+                        .filter(it -> it.getIgnoreType() == IgnoreType.HARD)
+                        .map(Ignore::getName)
+                        .toList());
     }
 
     public JsonChatPlayer toJsonChatPlayer(JsonChatPlayerRepository chatPlayerRepository) {
-        return new JsonChatPlayer(chatPlayerRepository, uniqueId, name, ignoreList);
+        return new JsonChatPlayer(chatPlayerRepository, uniqueId, name,
+                ignoreList.stream().map(it -> new Ignore(it, IgnoreType.HARD)).toList());
     }
 }
