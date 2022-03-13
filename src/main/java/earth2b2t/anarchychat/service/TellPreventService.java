0185a640612e575ca8c44e345c0cc76aaab37a31
Toshimichi0915 <main@toshimichi.net>
2022-03-13 11:16:55 +0900

    Add aliases

--- src/main/java/earth2b2t/anarchychat/service/TellPreventService.java
+++ src/main/java/earth2b2t/anarchychat/service/TellPreventService.java
@@ -4,16 +4,21 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class TellPreventService implements Service {
+
+    private static final List<String> VANILLA_TELL_COMMANDS = List.of(
+            "/minecraft:msg",
+            "/minecraft:tell",
+            "/minecraft:w"
+    );
 
     @EventHandler
     public void onAsyncPlayerChat(PlayerCommandPreprocessEvent e) {
 
         String msg = e.getMessage();
-        if (!msg.startsWith("/minecraft:msg") && !msg.startsWith("/minecraft:tell")) {
-            return;
-        }
+        if (!VANILLA_TELL_COMMANDS.contains(msg)) return;
 
         String[] split = msg.split(" ");
         String args = String.join(" ", Arrays.copyOfRange(split, 1, split.length));
